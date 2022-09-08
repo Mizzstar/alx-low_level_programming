7c870627ec773161cc6505077aabb5afbff0f890
Esther Udeh <udehesther25@gmail.com>
2022-09-08 07:22:34 -0700

    learning

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
 /**
  * main - Short description, single line
  * Description: Longer description of the function)?
- *
  * Return: Description of the returned value
  */
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
