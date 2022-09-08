4d52c5c1fcd4a3cd1d58de42c4f5e414dd583ebe
Esther Udeh <udehesther25@gmail.com>
2022-09-08 03:58:48 -0700

    Learning low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - entry point for the program
+ *
+ * Return: Displays a message to the terminal.
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
