1a235a18ae8cd9d8fdd8f1a7bc6cd47f3cf2c6ea
Esther Udeh <udehesther25@gmail.com>
2022-09-08 17:53:35 -0700

    C coding

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <unistd>
+
 /**
- * main - prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to standard error.
+ * Reture: Always 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+
 	return (1);
 }
