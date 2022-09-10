6dbcd00ddf2d9aeeb054ad5d96f4716b1ee1a27d
Esther Udeh <udehesther25@gmail.com>
2022-09-09 18:27:51 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.>
+
+/**
+ * main - Entry point
+ * Description: prints all the numbers thatmake up the 
+ * decimal number system and then prints newline.
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0; + num);
+	}
+	
+	puchar ('\n');
+
+	return (0);
+}
