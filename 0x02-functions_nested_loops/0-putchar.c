b6bb82446a392b188403f0515b5df090dcb357de
Esther Udeh <udehesther25@gmail.com>
2022-09-13 09:55:26 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (If successful)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_puchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
