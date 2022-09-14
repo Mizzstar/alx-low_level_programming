4c77d47e15dc5d2d7fb164a4e082aecc00894f75
Esther Udeh <udehesther25@gmail.com>
2022-09-14 01:53:33 -0700

    Programming

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int count;
-	unsigned long fib1 = 0, fib2 =1, sum;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
 	for (count = 0; count < 50; count++)
 	{
@@ -25,5 +25,5 @@ int main(void)
 			printf(", ");
 	}
 
-	return(0);
+	return (0);
 }
