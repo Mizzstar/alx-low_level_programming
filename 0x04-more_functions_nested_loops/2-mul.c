a193778586d7c6889a75feb65b7c44e9539c092c
Esther Udeh <udehesther25@gmail.com>
2022-09-15 09:50:34 -0700

    working now

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * mul - Multiples teo integers.
+ * mul - Multiples two integers.
  * @a: The first integer to be multiplied.
  * @b: The second integer to be multiplied.
  *
