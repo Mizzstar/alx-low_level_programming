06f3d29d16f19739a2ba91f2ad390f14b05af525
Esther Udeh <udehesther25@gmail.com>
2022-09-30 08:53:27 -0700

    coding

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argc: the number of arguments supplied to the program.
  * @argv: an array of pointers to the arguments.
  *
- * Return: always o.
+ * Return: always 0.
  */
 int main(int __attribute__((__unused__)) argc, char *argv [])
 {
