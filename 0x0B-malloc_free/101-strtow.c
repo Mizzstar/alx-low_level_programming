78111d049de9ef70d47c2924cc401edad1d8a731
Esther Udeh <udehesther25@gmail.com>
2022-10-03 10:25:48 -0700

    i will show you how great I am

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,46 @@
+#include " main.h"
+#include <stdlib.h>
+
+/**
+ * word_len - locates the index marking the end of the
+ * first word contained within a string.
+ * @str: the string to be searched.
+ *
+ * Return: the index marint the end of the initial word pointed to by str.
+ */
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != ' ')
+	{
+		len++;
+		index++;
+	}
+
+	return (len);
+}
+
+/**
+ * count_words _ counts the number of words contained within a string.
+ * @str: the string to be searched.
+ *
+ * Return: the number of words contained within str.
+ */
+int count_words(char *str)
+{
+	int index = 0, len = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+	return (words);
+}
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free a 2-dimensional array of integers.
+ * @grid: the 2-dimensional array of integers to be freed.
+ * @height: the height of grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int index;
+
+	for (index = 0; index < height; index++)
+		free(grid[index]);
+	free(grid);
+}
