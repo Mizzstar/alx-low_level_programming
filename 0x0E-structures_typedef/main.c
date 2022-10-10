256badd2bd85f12d4be72a7c563ea565711676b4
Esther Udeh <udehesther25@gmail.com>
2022-10-10 08:32:33 -0700

    coding

---
+++ 0x0E-structures_typedef/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bod";
+	printf("my name is %s, and I am %.1f :) - woof!\n", my-dog.name, my_dog.age);
+	return (0);
+}
