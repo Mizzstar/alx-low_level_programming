080d5fcf7d636dfb650866ae2859013fe7c5f37b
Esther Udeh <udehesther25@gmail.com>
2022-10-25 10:00:44 -0700

    listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* malloc, free */
+#include <string.h> /* size_t */
 
 /**
  * struct listint_s - singly linked list
