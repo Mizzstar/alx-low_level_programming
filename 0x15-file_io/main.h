db9856d7d3be7d74b637cd1f50577596e63a1daf
Esther Udeh <udehesther25@gmail.com>
2022-10-31 18:18:00 -0700

    codes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _FILE_IO_
+#define _FILE_IO_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,7 +10,9 @@
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_file(const char *src, const char *dest);
 
 #endif /* MAIN_H */
