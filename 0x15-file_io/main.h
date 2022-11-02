cd103838abeedfe54c15c3bb8faca15ebf857310
Esther Udeh <udehesther25@gmail.com>
2022-11-02 02:39:36 -0700

    list

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,12 @@
 #ifndef _FILE_IO_
 #define _FILE_IO_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
