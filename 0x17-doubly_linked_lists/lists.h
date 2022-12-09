6f185147f1bffa0354590380fa37e51f97575bdd
Esther Udeh <udehesther25@gmail.com>
2022-12-09 02:21:29 -0800

    doubly linked

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -13,7 +13,7 @@
  * Description: doubly linked list node structure
  * for Holberton project
  */
-typedef struct dlistint-s
+typedef struct dlistint_s
 {
 	int n;
 	struct dlistint_s *prev;
@@ -21,7 +21,7 @@ typedef struct dlistint-s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-size_t dlistint_len(const dlistint- *h);
+size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
